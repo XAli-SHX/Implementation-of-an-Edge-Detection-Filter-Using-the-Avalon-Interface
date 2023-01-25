dbf5515d5a8cfbfb33f9a3831f09a7d3dad4db16
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-01-25 17:42:25 +0330

    Complete edge detection using software

--- Codes/edge_detection/software/EdgeDetector.py
+++ Codes/edge_detection/software/EdgeDetector.py
@@ -4,17 +4,26 @@ import numpy as np
 if __name__ == "__main__":
     rgb = Image.open("img_rgb.jpg")
     gray = rgb.convert("L")
-    grayPixels = []
     xSize, ySize = gray.size
-    for x in range(xSize):
-        for y in range(ySize):
-            grayPixels.append(gray.getpixel((x, y)))
-    grayPixels = np.array(grayPixels)
     kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-    ky = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-    for ix in range(0, xSize):
-        for iy in range(0, ySize):
+    ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    edge = []
+    for ix in range(xSize):
+        row = []
+        for iy in range(ySize):
             kxResult = 0
             kyResult = 0
-            for kx in range(-1, 2):
-                pass
+            for i in range(3):
+                for j in range(3):
+                    if ix + 2 >= xSize or iy + 2 >= ySize:
+                        continue
+                    kxResult += kx[i][j] * gray.getpixel((ix + i, iy + j))
+                    kyResult += ky[i][j] * gray.getpixel((ix + i, iy + j))
+            row.append((abs(kxResult) + abs(kyResult)) / 2)
+        edge.append(row)
+    edgeImage = Image.new("L", (xSize, ySize))
+    for x in range(xSize):
+        for y in range(ySize):
+            edgeImage.putpixel((x, y), int(edge[x][y]))
+    edgeImage.save("img_edge.jpg")
+    edgeImage.show()
