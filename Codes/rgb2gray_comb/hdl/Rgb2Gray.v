9053854598cbdfc1460395f2c7ee59472e0569e2
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-01-25 18:47:09 +0330

    Create a combinational rgb2gray module

---
+++ Codes/rgb2gray_comb/hdl/Rgb2Gray.v
@@ -0,0 +1,23 @@
+module Rgb2Gray (
+    clk_i, 
+    rst_i,
+    RedColor_i,
+    GreenColor_i,
+    BlueColor_i,
+    GrayColor_o
+);
+
+    input clk_i, rst_i;
+    input [7:0] RedColor_i, GreenColor_i, BlueColor_i;
+    output [7:0] GrayColor_o;
+
+    wire [9:0] SumResult_w;
+    assign SumResult_w = RedColor_i + GreenColor_i + BlueColor_i;
+
+    // Divide by 3
+    assign GrayColor_o = (SumResult_w >> 2) + 
+                         (SumResult_w >> 4) + 
+                         (SumResult_w >> 6) +
+                         (SumResult_w >> 7) ;
+    
+endmodule
