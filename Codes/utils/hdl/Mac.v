c27e709cb6ff14b30d237c6c105a596228e3266c
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-01-26 18:25:36 +0330

    Update MAC port sizing

--- Codes/utils/hdl/Mac.v
+++ Codes/utils/hdl/Mac.v
@@ -1,15 +1,22 @@
-module Mac (
+module Mac #(
+    parameter MULT_SIZE = 8,
+              ACCUM_SIZE = 8
+)(
     Mult1_i,
     Mult2_i,
     Accum_i,
     Result_o
 );
 
-    input [7:0] Mult1_i, Mult2_i, Accum_i;
-    output [7:0] Result_o;
+    localparam RESULT_SIZE = (MULT_SIZE+MULT_SIZE) > ACCUM_SIZE ? 
+                             (MULT_SIZE+MULT_SIZE+1) : (ACCUM_SIZE+1);
 
-    wire [7:0] Product;
-    wire [7:0] Sum;
+    input [MULT_SIZE-1:0] Mult1_i, Mult2_i;
+    input [ACCUM_SIZE-1:0] Accum_i;
+    output [RESULT_SIZE-1:0] Result_o;
+
+    wire [MULT_SIZE+MULT_SIZE:0] Product;
+    wire [RESULT_SIZE-1:0] Sum;
 
     assign Product = Mult1_i * Mult2_i;
     assign Sum = Product + Accum_i;
